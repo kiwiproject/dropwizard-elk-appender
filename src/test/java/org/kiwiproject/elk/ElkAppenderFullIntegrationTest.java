54f584059359533820508f15121fa358966d7a04
Scott Leberknight <174812+sleberknight@users.noreply.github.com>
2025-11-04 20:39:25 -0500

    Add integration tests and update existing test to use Testcontainers (#294)
    * Add integration tests: one "heavy" one that uses a full Logstash
    container and is only activated with a system property, and a
    "lightweight" one that uses netcat to capture the log messages in a
    lightweight container with netcat pre-installed. Both of these extend an
    abstract base test, AbstractElkAppenderIntegrationTest.
    * Update existing ElkAppenderFactoryTest to use the lightweight
    container, so that the TCP tests don't throw exceptions when they get
    "connection refused". This didn't cause the tests to fail, but is
    confusing when you see a bunch of stack traces where you aren't
    expecting them in successful tests.
    * Add several test extensions to eliminate duplicated code across the
    tests: LogstashContainerExtension and Dropwizard TestAppExtension.
    * Add a logback-test.xml logging configuration for test code.
    * Add several test dependencies including dropwizard-testing support,
    dropwizard-core, kiwi-test, and testcontainers for junit jupiter.
    * Remove the (now redundant) .keep file from src/test/resources
    
    Closes #292
    Closes #293

---
+++ src/test/java/org/kiwiproject/elk/ElkAppenderFullIntegrationTest.java
@@ -0,0 +1,28 @@
+package org.kiwiproject.elk;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.condition.EnabledIfSystemProperty;
+import org.junit.jupiter.api.extension.RegisterExtension;
+
+@EnabledIfSystemProperty(named = "enableHeavyIntegrationTests", matches = "true")
+@DisplayName("ElkAppender (using Logstash in container)")
+@Slf4j
+class ElkAppenderFullIntegrationTest extends AbstractElkAppenderIntegrationTest {
+
+    @RegisterExtension
+    static final LogstashContainerExtension LOGSTASH = new LogstashContainerExtension();
+
+    @RegisterExtension
+    static final DropwizardTestAppExtension DW_APP = new DropwizardTestAppExtension();
+
+    @Override
+    protected LogstashContainerExtension logstash() {
+        return LOGSTASH;
+    }
+
+    @Override
+    protected DropwizardTestAppExtension dwApp() {
+        return DW_APP;
+    }
+}
---
+++ src/test/java/org/kiwiproject/elk/LogstashContainerExtension.java
@@ -0,0 +1,159 @@
+package org.kiwiproject.elk;
+
+import static java.util.Objects.nonNull;
+
+import lombok.Getter;
+import lombok.experimental.Accessors;
+import lombok.extern.slf4j.Slf4j;
+import org.jspecify.annotations.NonNull;
+import org.junit.jupiter.api.extension.AfterAllCallback;
+import org.junit.jupiter.api.extension.BeforeAllCallback;
+import org.junit.jupiter.api.extension.ExtensionContext;
+import org.kiwiproject.base.UncheckedInterruptedException;
+import org.testcontainers.containers.BindMode;
+import org.testcontainers.containers.Container;
+import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.containers.wait.strategy.Wait;
+import org.testcontainers.utility.DockerImageName;
+
+import java.io.IOException;
+import java.time.Duration;
+
+/**
+ * Creates a Logstash container for use by integration tests. The container is started before
+ * all tests and is stopped after all tests have completed.
+ * <p>
+ * This extension also sets the system properties required by {@link org.kiwiproject.config.provider.ElkLoggerConfigProvider}
+ * before all tests run, and clears them after all tests complete.
+ */
+@Slf4j
+public class LogstashContainerExtension implements BeforeAllCallback, AfterAllCallback {
+
+    private static final DockerImageName LOGSTASH_IMAGE = 
+            DockerImageName.parse("docker.elastic.co/logstash/logstash:9.2.0");
+
+    private static final DockerImageName NETCAT_IMAGE_NAME =
+            DockerImageName.parse("toolbelt/netcat:2025-10-23");
+
+    /**
+     * The type of Logstash container.
+     */
+    public enum LogstashContainerType {
+        
+        /**
+         * Uses a real Logstash container.
+         */
+        REAL,
+        
+        /**
+         * Simulates Logstash using netcat.
+         */
+        SIMULATED
+    }
+
+    @Getter
+    @Accessors(fluent = true)
+    private GenericContainer<?> container;
+
+    private final LogstashContainerType containerType;
+
+    /**
+     * Create a container that uses a real Logtash container.
+     */
+    public LogstashContainerExtension() {
+        this(LogstashContainerType.REAL);
+    }
+
+    /**
+     * Create a container that uses the given container type to create a real
+     * or simulated Logstash container.
+     */
+    public LogstashContainerExtension(LogstashContainerType containerType) {
+        this.containerType = containerType;
+    }
+
+    @Override
+    public void beforeAll(@NonNull ExtensionContext context) {
+        container = switch (containerType) {
+            case REAL -> newLogstashContainer();
+            case SIMULATED -> newSimulatedLogstashContainer();
+        };
+
+        LOG.info("Starting Logstash");
+        container.start();
+
+         // Set properties for ElkLoggerConfigProvider
+        var host = container.getHost();
+        var port = container.getMappedPort(5044).toString();
+        LOG.info("Container host {} started with port {} mapped to container 5044", host, port);
+        
+        System.setProperty("kiwi.elk.host", host);
+        System.setProperty("kiwi.elk.port", port);
+    }
+
+    @SuppressWarnings("resource")
+    private static GenericContainer<?> newLogstashContainer() {
+        return new GenericContainer<>(LOGSTASH_IMAGE)
+                .withClasspathResourceMapping(
+                    "logstash.conf", "/usr/share/logstash/pipeline/logstash.conf", BindMode.READ_ONLY)
+                .withExposedPorts(5044)
+                .waitingFor(Wait.forListeningPort())
+                .withStartupTimeout(Duration.ofSeconds(60));
+    }
+
+    @SuppressWarnings("resource")
+    private static GenericContainer<?> newSimulatedLogstashContainer() {
+        return new GenericContainer<>(NETCAT_IMAGE_NAME)
+                .withCreateContainerCmdModifier(cmd -> cmd.withEntrypoint("/bin/sh"))
+                .withCommand("-c", "nc -l -k -p 5044 > /tmp/logs.txt")
+                .withExposedPorts(5044)
+                .waitingFor(Wait.forListeningPort())
+                .withStartupTimeout(Duration.ofSeconds(30));
+    }
+
+    @Override
+    public void afterAll(@NonNull ExtensionContext context) {
+        System.clearProperty("kiwi.elk.host");
+        System.clearProperty("kiwi.elk.port");
+
+        if (nonNull(container)) {
+            LOG.warn("Stopping Logstash");
+            container.stop();
+        }
+    }
+
+    /**
+     * Get the container logs when the container is a real Logstash container, or the
+     * netcat logs when the container is a simulated Logstash container.
+     */
+    public String logs() {
+        return switch (containerType) {
+            case REAL -> container.getLogs();
+            case SIMULATED -> execInContainerReturningStdOut("cat", "/tmp/logs.txt");
+        };
+    }
+
+    /**
+     * Convenience method to execute a command in the container and return its standard output.
+     */
+    public String execInContainerReturningStdOut(String... command) {
+        return execInContainer(command).getStdout();
+    }
+
+    /**
+     * Convenience method to execute a command in the container and return its result.
+     * 
+     * @implNote wraps the checked exceptions thrown by {@link Container#execInContainer(String...)}
+     * in a runtime exception.
+     */
+    public Container.ExecResult execInContainer(String... command) {
+        try {
+            return container.execInContainer(command);
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+            throw new UncheckedInterruptedException(e);
+        } catch (UnsupportedOperationException | IOException e) {
+           throw new RuntimeException(e);
+        }
+    }
+}
