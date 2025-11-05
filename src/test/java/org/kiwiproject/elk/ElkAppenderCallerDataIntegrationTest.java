7a64a1b017919d4b049a7857909552a3969efd4a
Scott Leberknight <174812+sleberknight@users.noreply.github.com>
2025-11-04 22:47:06 -0500

    Fix missing caller data when includeCallerData is true
    * Update ElkAppenderFactory#build to call setIncludeCallerData
      on the superclass (AbstractAppenderFactory). This tells
      Dropwizard to ensure caller data is populated.
    * Add ElkAppenderCallerDataIntegrationTest to verify when
      includeCallerData is true.
    * Add a test in ElkAppenderIntegrationTest to verify the default
      behavior, which is to NOT include caller data.
    
    Closes #300

--- src/test/java/org/kiwiproject/elk/ElkAppenderCallerDataIntegrationTest.java
+++ src/test/java/org/kiwiproject/elk/ElkAppenderCallerDataIntegrationTest.java
@@ -2,27 +2,25 @@ package org.kiwiproject.elk;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.awaitility.Awaitility.await;
+import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.kiwiproject.test.constants.KiwiTestConstants.JSON_HELPER;
 
-import lombok.extern.slf4j.Slf4j;
 import org.awaitility.Durations;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.RegisterExtension;
-import org.kiwiproject.elk.LogstashContainerExtension.LogstashContainerType;
 
-@DisplayName("ElkAppender (using simulated Logstash)")
-@Slf4j
-class ElkAppenderIntegrationTest extends AbstractElkAppenderIntegrationTest {
+@DisplayName("ElkAppender (caller data)")
+class ElkAppenderCallerDataIntegrationTest extends AbstractElkAppenderIntegrationTest {
 
     @RegisterExtension
     static final LogstashContainerExtension LOGSTASH = LogstashContainerExtension.builder()
-            .containerType(LogstashContainerType.SIMULATED)
+            .containerType(LogstashContainerExtension.LogstashContainerType.SIMULATED)
             .build();
 
     @RegisterExtension
     static final DropwizardTestAppExtension DW_APP =
-            new DropwizardTestAppExtension("elk-integration-test-config.yml");
+            new DropwizardTestAppExtension("elk-caller-data-test-config.yml");
 
     @Override
     protected LogstashContainerExtension logstash() {
@@ -35,28 +33,26 @@ class ElkAppenderIntegrationTest extends AbstractElkAppenderIntegrationTest {
     }
 
     @Test
-    void shouldNotIncludeCallerDataByDefault() {
+    void shouldIncludeCallerData() {
         var logger = dwApp().getIntegrationTestLogger();
-        logger.info("nuqneH dropwizard!");
+        logger.info("Hej fra Dropwizard!");
 
         // Verify we saw the message
         await().atMost(Durations.TEN_SECONDS)
-                .untilAsserted(() -> assertThat(logstash().logs()).contains("nuqneH dropwizard!"));
+                .untilAsserted(() -> assertThat(logstash().logs()).contains("Hej fra Dropwizard!"));
 
         // Verify details of the log message
         var helloLog = logstash().logs().lines()
-                .filter(line -> line.contains("nuqneH dropwizard!"))
+                .filter(line -> line.contains("Hej fra Dropwizard!"))
                 .map(JSON_HELPER::toMap)
                 .findFirst()
                 .orElseThrow();
 
-        System.out.println("helloLog = " + helloLog);
-
-        assertThat(helloLog).doesNotContainKeys(
-                "caller_class_name",
-                "caller_method_name",
-                "caller_file_name",
-                "caller_line_number"
+        assertAll(
+                () -> assertThat(helloLog).containsEntry("caller_class_name", ElkAppenderCallerDataIntegrationTest.class.getName()),
+                () -> assertThat(helloLog).containsEntry("caller_method_name", "shouldIncludeCallerData"),
+                () -> assertThat(helloLog).containsEntry("caller_file_name", ElkAppenderCallerDataIntegrationTest.class.getSimpleName() + ".java"),
+                () -> assertThat(helloLog).containsKey("caller_line_number")
         );
     }
 }
